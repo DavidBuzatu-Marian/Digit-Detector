8826ead94341d3ee1cd41e80c0ad5652e6d183c8
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-22 13:14:13 +0300

    Added SGD method in neural networks

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -5,13 +5,19 @@ class NeuralNetwork:
     # constructor
     # get the number of layer sizes as a paramater and then
     # 1. initialize the weight matrix dimensions
-    # 2. initialize the weight matrixes with values close to [-1, 1]
-    # 3. initialize biases with zeros
+    # 2. set the number of layers of the network
+    # 3. initialize the weight matrixes with values close to [-1, 1]
+    # 4. initialize biases randomly
     def __init__(self, layer_sizes):
         weight_dims = [(a, b) for a, b in zip(layer_sizes[1:], layer_sizes[:-1])]
 
-        self.weights = [np.random.standard_normal(s) / s[1] ** .5 for s in weight_dims]
-        self.biases = [np.zeros((s, 1)) for s in layer_sizes[1:]]
+        self.nr_layers = len(layer_sizes)
+
+        # self.weights = [np.random.standard_normal(s) / s[1] ** .5 for s in weight_dims]
+        self.weights = [np.random.randn(s[0], s[1]) for s in weight_dims]
+        # self.biases = [np.zeros((s, 1)) for s in layer_sizes[1:]]
+        self.biases = [np.random.randn(s, 1) for s in layer_sizes[1:]]
+        print(self.weights[0])
 
     # predict function
     # get the matrix a as a parameter
@@ -23,6 +29,29 @@ class NeuralNetwork:
             z = np.matmul(weight, a) + bias
             a = self.activation(z)
         return a
+
+    # SGD method
+    # it uses stochastic gradient descent to learn the parameters
+    # it uses code from http://neuralnetworksanddeeplearning.com/chap1.html
+    # it gets a list of tuples (training_data) and it can show a progress if test_data is provided
+    def SGD(self, training_data, epochs, mini_batch_size, learning_rate,
+            test_data = None):
+            # check if test data exists and take its length
+            if (test_data):
+                nr_test = len(test_data)
+            size_of_training = len(training_data)
+            for j in range(epochs):
+                random.shuffle(training_data)
+                mini_batches = [
+                    training_data[k:k + mini_batch_size for k in range(0, size_of_training, mini_batch_size)]
+                ]
+                for mini_batch in mini_batches:
+                    self.update_mini_batch(mini_batch, learning_rate)
+                if test_data:
+                    print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), nr_test)
+                else:
+                    print "Epoch {0} complete".format(j)
+
 
     def print_accuracy(self, images, labels):
         predictions = self.predict(images)
--- test-neuralnets.py
+++ test-neuralnets.py
@@ -2,10 +2,10 @@ import numpy as np
 import neuralnetwork as nn
 
 
-layer_sizes = (1000, 300, 5)
+layer_sizes = (5, 3, 6)
 x = np.ones((layer_sizes[0], 1))
 
 network = nn.NeuralNetwork(layer_sizes)
 prediction = network.predict(x)
 
-print(prediction)
+# print(prediction)
