7d9302236f8ad0406f6062fb848a61b54cfdff8f
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-22 15:17:11 +0300

    Fixed untracked files

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import neuralnetwork as nn
 
-path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mnist.npz')
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../dataset/mnist.npz')
 with np.load(path) as data: 
     training_images = data['training_images']
     training_labels = data['training_labels']
